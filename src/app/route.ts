bf45c9089c26a95a8f36e4f63970ee5ca30c5f87
lipeng <937681195@qq.com>
2018-07-12 17:22:16 +0800

    asd

--- src/app/route.ts
+++ src/app/route.ts
@@ -3,12 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { OneComponent } from './one/one.component';
 import { TwoComponent } from './two/two.component';
-
+import { ThreeComponent } from './three/three.component';
+import { FourComponent } from './four/four.component'
 
 
 const routes: Routes = [
     { path: 'one', component: OneComponent },
     { path: 'two', component: TwoComponent },
+    { path: 'three', component: ThreeComponent },
+    { path: 'four', component: FourComponent },
 ];
 
 @NgModule({
